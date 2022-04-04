cc6377b6a67d585eb64b80b471a67dbd29318502
jonah <jonah@Jonah-Thompsons-Swift-X>
2022-04-04 15:10:12 -0600

    created the second page which is mostly done

--- Assignment 3/src/mru/tsc/view/GUIController.java
+++ Assignment 3/src/mru/tsc/view/GUIController.java
@@ -11,10 +11,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import mru.tsc.controller.StoreMenu;
+import mru.tsc.exceptions.ClassificationException;
+import mru.tsc.exceptions.PuzzleException;
+import mru.tsc.exceptions.SizeException;
+import mru.tsc.model.Animals;
+import mru.tsc.model.BoardGames;
+import mru.tsc.model.Figures;
+import mru.tsc.model.Puzzles;
 import mru.tsc.model.Toys;
 
 public class GUIController implements Initializable{
@@ -53,11 +61,13 @@ public class GUIController implements Initializable{
 	@FXML 
 	private TextField bgMin;
 	@FXML 
-	private TextField bgCMax;
+	private TextField bgMax;
 	@FXML 
 	private TextField bgDesign;
 	@FXML
 	private Button addSave;
+	@FXML
+	private Label addError;
 	
 	
 	StoreMenu st;
@@ -78,6 +88,9 @@ public class GUIController implements Initializable{
 		}
 		
 		category.getItems().addAll("Figures", "Animal", "Puzzle", "Board Game");
+		
+		//sets the addError message to an empty string
+		addError.setText("");
 		
 		
 		
@@ -103,6 +116,125 @@ public class GUIController implements Initializable{
 			}
 			
 		}
+		st.exit();
+	}
+	
+	public void addClicked(ActionEvent e) {
+		
+		long sn = 0;
+		String name = "";
+		String brand = "";
+		double price = 0;
+		int count = 0;
+		String age = "";
+		
+		
+		
+		try{
+		//setting the user input equal to the variables
+		sn = Long.parseLong(addSN.getText());
+				
+		name = addName.getText();
+			
+		brand = addBrand.getText();
+				
+		price = Double.parseDouble(addPrice.getText());
+				
+		count = Integer.parseInt(addCount.getText());
+				
+		age = addAge.getText();
+		
+		addError.setText("");
+				
+		}
+		catch(NumberFormatException e1) {
+			addError.setText("Invalid Input");
+				
+		}
+		catch(Exception e2) {
+			addError.setText("Invalid Input");
+				
+		}
+		
+		
+		
+		
+		if (category.getValue().equals("Figure")) {
+				
+			char classification = figClass.getText().charAt(0);	
+			
+			Toys t = null;
+			try {
+				t = new Figures(sn, name, brand, price, count, age, classification);
+			} catch(ClassificationException e1) {
+				
+				addError.setText("Invalid Classification");
+			}
+			
+			
+			
+			toys.add(t);
+			st.exit();
+			addError.setText("Toy Added");
+			
+		}
+		else if (category.getValue().equals("Animal")) {
+			
+			String material = animalMat.getText();
+			char size = animalSize.getText().charAt(0);
+			
+			Toys t = null;
+			try {
+				t = new Animals(sn, name, brand, price, count, age, material, size);
+			} catch (SizeException e1) {
+				
+				addError.setText("Invalid Size");
+			}
+			toys.add(t);
+			st.exit();
+			addError.setText("Toy Added");
+			
+		}
+		else if (category.getValue().equals("Puzzle")) {
+			
+			char type = puzzleType.getText().charAt(0);
+			
+			Toys t = null;
+			try {
+				t = new Puzzles(sn, name, brand, price, count, age, type);
+			} catch (PuzzleException e1) {
+				
+				addError.setText("Invalid Type");
+			}
+			toys.add(t);
+			st.exit();
+			addError.setText("Toy Added");
+			
+		}
+		else if (category.getValue().equals("Board Game")) {
+			
+			int min = 0;
+			int max = 0;
+			
+			try {
+				
+				min = Integer.parseInt(bgMin.getText());
+				max = Integer.parseInt(bgMax.getText());
+				
+			} catch (NumberFormatException e1) {
+				addError.setText("Invalid Max or Min");
+			}
+			
+			
+			String designers = bgDesign.getText();
+			
+			Toys t = new BoardGames(sn, name, brand, price, count, age, min, max, designers);
+			
+			toys.add(t);
+			st.exit();
+			addError.setText("Toy Added");
+		}
+		
 	}
 
 	
